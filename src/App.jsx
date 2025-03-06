9fe87b479a39e13c81336ba045aeeae1ebb584f9
James Joseph <98562132+JW796@users.noreply.github.com>
2025-03-06 15:23:36 -0500

    Login setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 function App() {
-
   return (
     <>
-    <h1> useReducer </h1>
+      <h1 className="text-center text-3xl font-semibold mt-2 py-4">
+        {" "}
+        useReducer
+      </h1>
+      <form action="">
+        <input
+          type="text"
+          autoComplete="username"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
